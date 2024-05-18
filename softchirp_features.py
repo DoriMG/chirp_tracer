d0c14401537d5e5df22ce460ea72937593a417e4
DoriMG <15670245+DoriMG@users.noreply.github.com>
2024-05-18 20:49:18 +0200

    Replaced wiener entropy

--- softchirp_features.py
+++ softchirp_features.py
@@ -6,10 +6,16 @@ from librosa.core import piptrack
 import librosa
 import pandas as pd
 
-def wiener_entropy(s_npy,  n_fft = 2048):
+def wiener_entropy(s_npy,  samplerate, n_fft = 512, low_pass = 2000, high_pass = 8000):
     """ Computes Wiener entropy of wav """
-    ent = librosa.feature.spectral_flatness(y = s_npy, n_fft = n_fft)
+
+    freqs = librosa.fft_frequencies(sr=samplerate, n_fft=n_fft)
+    inc = np.logical_and(freqs>low_pass, freqs<high_pass)
+    
+    S, phase = librosa.magphase(librosa.stft(s_npy,n_fft=n_fft, hop_length=256))
+    ent = librosa.feature.spectral_flatness(S=S[inc,:])
     return np.var(ent), np.mean(ent), np.max(ent), ent
+
 
 def spectral_contrast(s_npy, n_fft=2048):
     contrast = librosa.feature.spectral_contrast(y=s_npy, n_fft=n_fft)
@@ -34,7 +40,8 @@ def compute_amplitude(s_npy, win_length = 220):
     return np.mean(amps)
 
 def compute_wiener_ent(s_npy, win_length = 220):
-    """ Computes Wiener entropy of wav """
+    """ Computes Wiener entropy of wav
+     NOTE: This should not be used, I left it in for backwards compatability with AB's paper """
     ent = librosa.feature.spectral_flatness(y = s_npy, win_length = win_length)
     return np.sum(ent)
 
@@ -43,7 +50,7 @@ def compute_zero_crossing(s_npy,):
     return np.mean(librosa.zero_crossings(s_npy, pad=False))
 
 def compute_duration(call):
-    """Computes duration of the trace (as iamge). Requires wav sound. """
+    """Computes duration of the trace (as image). Requires wav sound. """
     return call['end_time']-call['start_time']
 
 def compute_height(t):
@@ -65,12 +72,11 @@ def compute_slope(line):
 def compute_max_freq(line):
     return np.max(line[:,1])
 
-def compute_all_features(calls, samplerate, freq):
+def compute_all_features(calls, samplerate, freq, n_fft=256):
     """ Computes all the features above for list of traces and corresponding wavs """
-    features = ['slope', 'frequency', 'pitch', 'amplitude', 'wiener_entropy',
-                'zero_crossings', 'duration', 'height', 'asymmetry',
-                 'entropy_variance', 'mean_entropy', 'maximum_entropy', 
-                 'mean_contrast','var_contrast', 'goodness_of_pitch']
+    features = ['slope', 'frequency', 'pitch', 'amplitude', 
+                                            'wiener_entropy', 'zero_crossings', 'duration', 
+                                            'height', 'asymmetry', 'entropy_variance', 'mean_entropy', 'maximum_entropy', 'mean_contrast','var_contrast', 'goodness_of_pitch']
     fail_count = 0
     for newcol in features:
         calls[newcol]=np.nan
@@ -90,8 +96,8 @@ def compute_all_features(calls, samplerate, freq):
             calls.at[index,'duration'] = compute_duration(call)
             calls.at[index,'height'] = compute_height(t)
             calls.at[index,'asymmetry'] = compute_asymmetry(t)
-
-            ent_var, mean_ent, max_ent,  ent = wiener_entropy(s, n_fft=256) 
+            
+            ent_var, mean_ent, max_ent,  ent = wiener_entropy(s,samplerate, n_fft=512, low_pass = 2000, high_pass = 8000) 
             calls.at[index,'entropy_variance'] = ent_var
             calls.at[index,'mean_entropy'] = mean_ent
             calls.at[index,'maximum_entropy'] = max_ent
